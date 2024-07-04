0d11087403042c6e00aa82ea4630f67b9857d8cb
ABDULLAHMUHAMMADK <m.abdullah.khan00099@gmail.com>
2024-07-05 02:19:28 +0500

    pushing code

---
+++ ass_6.js
@@ -0,0 +1,49 @@
+// part_1
+// let part_1 = " Hello World ";
+// let trim = part_1.trim();
+// alert(trim);
+
+// part_2
+// let part_2 = "Hello World";
+// let x = part_2.substring(5, 11);
+// alert(x);
+
+// part_3
+// let part_3 = "Hello World";
+// x = part_3.substr(0, 5);
+// alert(x);
+
+// part_4
+// let part_4 = "Hello-World";
+// let x = part_4.split("-");
+// alert(x);
+
+// part_5
+// let part_5 = ["Hello","World"];
+// let x = part_5.join(" ");
+// alert(x);
+
+// part_6
+// let part_6 = "Hello World";
+// let x = part_6.indexOf("o");
+// alert(x);
+
+// part_7
+// let part_7 = "Hello World";
+// let x = part_7.lastIndexOf("World");
+// alert(x);
+
+// part_8
+// let part_8 = "World";
+// let x = part_8.startsWith("Wor");
+// alert(x);
+
+// part_9
+// let part_9 = "Hello World";
+// let x = part_9.endsWith("World");
+// alert(x);
+
+// part_10
+// let part_10 = "Hello World";
+// let x = part_10.includes("o");
+// alert(x);
